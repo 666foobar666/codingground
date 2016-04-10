f1baeaf542490f2b09069af791742c8e62e9b882
666foobar666 <666foobar666@gmail.com>
2016-04-09 20:54:55 -0400

    Update warmUpQ02.java

--- A4/warmUpQ02.java
+++ A4/warmUpQ02.java
@@ -1,27 +1,39 @@
 import java.util.ArrayList;
 import java.io.*;
 
-public class warmUpQ02{
+public class warmUpQ02
+{
+   public static void main(String[] args)
+   {
+      ArrayList<String> l_words = new ArrayList<String>();
     
-    public static void readFile(String name){
-    try{
-      FileReader fr = new FileReader(name);
-      BufferedReader br = new BufferedReader(fr);
-      String line = br.readLine();
-      while(line != null){
-        System.out.println(line);
-        line = br.readLine();
-      }
-      br.close();
-      fr.close();
-    }catch(IOException e){
-    }
+      l_words = readFileToArrayList("HelloWorld.java");
     
-    public ArrayList<String> readFileToArrayList(String p_fileName){
-        for(String word: words){
-        bw.write(word+"\n");
-      }
-    }
+      printArrayList(l_words);
+   }  
+  
+    
+   public static ArrayList<String> readFileToArrayList(String p_fileName)
+   {
+      ArrayList<String> l_returnWords = new ArrayList<String>();
+      
+      // step1: iterate through the file
+      // step2: grap a line from a file
+      // step3: assign the line to an ArrayList
+      
+      
+      
+      
+      return( l_returnWords );
+   }
+   
+   public static void printArrayList(ArrayList<String> p_words)
+   {
+      // step1: iterate through ArrayList
+      // step2: print to console each element
+      
+      
+   }
     
     
 }
