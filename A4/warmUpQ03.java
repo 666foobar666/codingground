326112b0805cb8a1f4d1722e11a65fdfeef94d97
666foobar666 <666foobar666@gmail.com>
2016-04-10 17:53:02 -0400

    Created by Coding Ground Client

---
+++ A4/warmUpQ03.java
@@ -0,0 +1,53 @@
+import java.util.ArrayList;
+import java.io.*;
+
+public class warmUpQ03
+{
+   public static void main(String[] args)
+   {
+      ArrayList<String> l_words = new ArrayList<String>();
+    
+      l_words = readFileToArrayList("HelloWorld.java");
+    
+      printArrayList(l_words);
+   }  
+   
+   public static ArrayList<String> readFileToArrayList(String p_fileName)
+   {
+      ArrayList<String> l_returnWords = new ArrayList<String>();
+      try
+      {
+         // step0: open file and read it into a buffer
+         FileReader l_fr = new FileReader(p_fileName);
+         BufferedReader l_br = new BufferedReader(l_fr);
+         String l_line = l_br.readLine();
+            
+         // step1: iterate through the file
+         while(l_line != null){
+            // step2: assign the line to an ArrayList
+            l_returnWords.add(l_line);
+            // step3: grap a line from a file
+            l_line = l_br.readLine();
+         }
+      }
+      catch(IOException e)
+      {
+         e.printStackTrace();
+      }
+      
+      return( l_returnWords );
+   }
+   
+   public static void printArrayList(ArrayList<String> p_words)
+   {
+      // step1: iterate through ArrayList
+      for(String l_word: p_words){
+         // step2: print to console each element
+         System.out.println(l_word);
+      }
+     
+      
+   }
+    
+    
+}
